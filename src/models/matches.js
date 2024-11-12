345e60ddcecbf533c9844f7bd6bc9200d86b758d
Proskauer <nicoprosk@gmail.com>
2024-11-12 16:57:35 -0500

    added mongoose, put some notes down on structure

---
+++ src/models/matches.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose');
+
+const matchesSchema = new Schema({
+  week: { type: Number, required: true },
+  role1: { type: String, required: true },
+  role2: { type: String, required: true },
+  status: { type: String, required: false, default: 'pending' },
+});
+
+module.exports = model('Matches', matchesSchema);
